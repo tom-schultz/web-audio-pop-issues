161a5bfff0f3d04de3591ea9038ba4b59ca54fe3
Tom Schultz <2112416+tom-schultz@users.noreply.github.com>
2026-05-25 22:42:47 -0400

    Instructions and multiple ways to play sound.

--- audio_pops.gd
+++ audio_pops.gd
@@ -2,24 +2,24 @@ extends AudioStreamPlayer
 
 @export var _stream : AudioStream
 
-var _play : bool
+var _play_self : bool
 
 func _ready() -> void:
 	stream = _stream
 	finished.connect(_finished)
 
 func _process(_delta: float) -> void:
-	_play = Input.is_action_pressed("play")
+	_play_self = Input.is_key_pressed(KEY_CTRL)
 	
-	#if Input.is_action_just_pressed("play"):
-		#var child = AudioStreamPlayer.new()
-		#add_child(child)
-		#child.finished.connect(func(): child.queue_free())
-		#child.stream = _stream
-		#child.play()
+	if Input.is_action_just_pressed("play"):
+		var child = AudioStreamPlayer.new()
+		add_child(child)
+		child.finished.connect(func(): child.queue_free())
+		child.stream = _stream
+		child.play()
 		
-	if not playing and _play:
+	if not playing and _play_self:
 		play()
 
 func _finished() -> void:
-	if _play: play()
+	if _play_self: play()
